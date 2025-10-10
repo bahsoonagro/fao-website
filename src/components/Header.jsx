a2b957c615ceb6d545e2f6b24af355be95344396
bahsoonagroprocessingindustries. <37225677+bahsoonagro@users.noreply.github.com>
2025-10-10 14:04:25 +0000

    Update Header.jsx

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,7 +3,7 @@ import Navbar from "./Navbar";
 
 const Header = () => {
   return (
-    <header className="bg-green-700 text-white">
+    <header className="bg-blue-700 text-white">
       <div className="container mx-auto flex justify-between items-center p-4">
         <h1 className="text-xl font-bold">FAO Sierra Leone</h1>
         <Navbar />
